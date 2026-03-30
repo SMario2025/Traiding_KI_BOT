bc1fd1f73fafc0c1a667a8d6a29074505152ca0c
SMario2025 <pascal-merzenich@web.de>
2026-03-30 03:05:18 +0200

    Delete Bot_github_railway_ready_ai_visible directory

--- Bot_github_railway_ready_ai_visible/dist/scan-test.js
+++
@@ -1,8 +0,0 @@
-import { scanSafeCandidates } from "./scanner.js";
-async function main() {
-    const rows = await scanSafeCandidates();
-    for (const r of rows.slice(0, 15)) {
-        console.log(`${r.symbol} | mint=${r.mint} | liq=${r.liquidityUsd} | vol=${r.volume24hUsd} | ageMin=${r.ageMinutes} | score=${r.score} | ${r.reasons.join(",")}`);
-    }
-}
-main().catch(console.error);
